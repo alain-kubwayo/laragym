22e44d7a0f8cae0291abb65cfef2c54043fe152a
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-08 21:10:52 +0200

    feat: complete laravel api and configure vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,14 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// import { createApp } from 'vue';
+import { createApp } from 'vue/dist/vue.esm-bundler';
+import router from './router';
+import CompaniesIndex from './components/companies/CompaniesIndex.vue'
+
+createApp({
+    components: {
+        CompaniesIndex
+    },
+}).use(router).mount('#app')
