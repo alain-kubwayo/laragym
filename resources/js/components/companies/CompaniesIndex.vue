336a35ad11dc432a37c5c85ae8d3fbf76d73f9c2
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-13 22:48:37 +0200

    feat: add companies list

--- resources/js/components/companies/CompaniesIndex.vue
+++ resources/js/components/companies/CompaniesIndex.vue
@@ -1,3 +1,21 @@
 <template>
-    Hello World.
+    <ul>
+        <li v-for="company in companies" :key="company.id">{{ company.name }} - {{ company.email }} - {{ company.address }} - {{ company.website }}</li>
+    </ul>
 </template>
+
+<script>
+import useCompanies from '../../composables/companies';
+import { onMounted } from 'vue';
+
+export default {
+    setup() {
+        const { companies, getCompanies } = useCompanies()
+        onMounted(getCompanies)
+        return {
+            companies
+        }
+    }
+}
+
+</script>
